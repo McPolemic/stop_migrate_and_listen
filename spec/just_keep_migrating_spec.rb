3b3acdf9851cfb3dff0d75db3f351bd752689db3
Adam Lukens <adam@testdouble.com>
2018-01-01 01:34:47 -0500

    Add test for veirfying rails server early bailing

--- spec/just_keep_migrating_spec.rb
+++ spec/just_keep_migrating_spec.rb
@@ -1,11 +1,41 @@
 require "spec_helper"
+require 'open3'
 
 RSpec.describe JustKeepMigrating do
+  before(:all) do
+    @script_setup = <<-BASH
+      cd example
+      bundle
+    BASH
+  end
+
   it "has a version number" do
     expect(JustKeepMigrating::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "with pending migrations" do
+    describe "starting a rails server" do
+      it "blocks and prints an error" do
+        stdout, stderr = shell("bundle exec rails server")
+        puts stdout
+        puts stderr
+        expect(stdout).to include "You have 1 pending migration"
+      end
+    end
+  end
+
+private
+
+  def shell(command)
+    script = <<-SCRIPT
+      export BUNDLE_GEMFILE="Gemfile"
+      export RAILS_ENV="development"
+      rm -f Gemfile.lock db/*.sqlite3
+      bundle install
+      #{command}
+    SCRIPT
+    Bundler.with_clean_env do
+      Open3.capture3(script, :chdir => "example")
+    end
   end
 end
